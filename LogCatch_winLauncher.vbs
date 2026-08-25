4b157bf61230ca76f0b237a2082b5fc7e1735aaf
Mitch Capper <mitch.capper@gmail.com>
2026-08-24 17:53:43 -0700

    More dynamic/error resistance setup scripts

--- LogCatch_winLauncher.vbs
+++ LogCatch_winLauncher.vbs
@@ -2,12 +2,21 @@ Set ws = CreateObject("Wscript.Shell")
 
 Set objFso = CreateObject("Scripting.FileSystemObject")
 
-Const SETUP_BATCH = "src\setup_path_for_windows.bat"
-Const RUN_BATCH = "src\logcatch.bat"
+' Resolve everything against the script's own folder: ws.Run inherits whatever
+' working directory the shortcut happened to be launched with, so relative
+' paths are not reliable here.
+Dim baseDir, SETUP_BATCH, RUN_BATCH
+baseDir = objFso.GetParentFolderName(WScript.ScriptFullName)
+SETUP_BATCH = objFso.BuildPath(baseDir, "src\setup_path_for_windows.bat")
+RUN_BATCH = objFso.BuildPath(baseDir, "src\logcatch.bat")
+
+' vbHide is a VBA constant, not a VBScript one - spell the value out.
+Const WINDOW_HIDDEN = 0
 
 If objFso.FileExists(SETUP_BATCH) Then
 
-    Private updateRunBatch
+    Dim updateRunBatch
+    updateRunBatch = False
     If objFso.FileExists(RUN_BATCH) Then
         Set runFile = objFso.GetFile(RUN_BATCH)
         Set setupFile = objFso.GetFile(SETUP_BATCH)
@@ -19,13 +28,15 @@ If objFso.FileExists(SETUP_BATCH) Then
     End If
 
     if updateRunBatch Then
-        ws.run "cmd /c " & SETUP_BATCH, vbhide
-        WScript.Sleep 2500
+        ' Wait for setup to finish instead of guessing at a sleep duration.
+        ws.Run "cmd /c """ & SETUP_BATCH & """", WINDOW_HIDDEN, True
     End If
 
 End If
 
 If objFso.FileExists(RUN_BATCH) Then
-    ws.run "cmd /c" & RUN_BATCH, vbhide
+    ws.Run "cmd /c """ & RUN_BATCH & """", WINDOW_HIDDEN, False
+Else
+    MsgBox "LogCatch could not be started: " & RUN_BATCH & " was not generated." & vbCrLf & vbCrLf & _
+           "Run src\setup_path_for_windows.bat manually to see why.", 16, "LogCatch"
 End If
-
--- src/setup_path_for_windows.bat
+++ src/setup_path_for_windows.bat
@@ -11,52 +11,69 @@ if EXIST %PATH_LIST% (
   del %PATH_LIST%
 )
 
+:: NOTE: batch has no "break" for a for-loop, and a :label inside a
+:: parenthesized block makes cmd.exe fail to parse the whole block
+:: ("Unexpected )"). "set /p" reads only the first line, which gives us
+:: the first-match-wins behaviour we want without any loop at all.
+
 set WISH_CMD="wish.exe"
-dir /s /b %SEARCH_PATH% | findstr "%WISH_CMD%$" > path_list.tmp
-for /f "delims=" %%i in (path_list.tmp) do (
-  echo "%%i"
-  echo %%i>> %PATH_LIST%
-  echo %%i> wish_path.list
+set WISH_PATH=
+dir /s /b %SEARCH_PATH% 2>nul | findstr "%WISH_CMD%$" > path_list.tmp
+if exist path_list.tmp set /p WISH_PATH=<path_list.tmp
+if defined WISH_PATH (
+  echo "%WISH_PATH%"
+  echo %WISH_PATH%>> %PATH_LIST%
+  echo %WISH_PATH%> wish_path.list
   echo @echo off > logcatch.bat
   echo cd /d %%~dp0 >> logcatch.bat
-  echo start "-" /B "%%i" src/LogCatch.tcl --dir src %%* >> logcatch.bat
+  echo start "-" /B "%WISH_PATH%" "%%~dp0LogCatch.tcl" %%* >> logcatch.bat
   set WISH_FOUND=1
-  break
 )
-:: exit /b 0
-del path_list.tmp
+if exist path_list.tmp del path_list.tmp
 
 
 set BASH_CMD="bash.exe"
-dir /s /b %SEARCH_PATH%\bin | findstr "%BASH_CMD%$" > path_list.tmp
-for /f "delims=" %%i in (path_list.tmp) do (
-  echo "%%i"
-  echo %%i>> %PATH_LIST%
-  echo %%i> bash_path.list
-
-  if not defined WISH_FOUND (
-    echo @echo off > logcatch.bat
-    echo cd /d %%~dp0 >> logcatch.bat
-    echo start "-" /B "%%i" -l -c "wish src/LogCatch.tcl --dir src %%*" >> logcatch.bat
-  )
-  break
+set BASH_PATH=
+dir /s /b %SEARCH_PATH%\bin 2>nul | findstr "%BASH_CMD%$" > path_list.tmp
+if exist path_list.tmp set /p BASH_PATH=<path_list.tmp
+if defined BASH_PATH (
+  echo "%BASH_PATH%"
+  echo %BASH_PATH%>> %PATH_LIST%
+  echo %BASH_PATH%> bash_path.list
 )
-del path_list.tmp
+if defined BASH_PATH if not defined WISH_FOUND (
+  echo @echo off > logcatch.bat
+  echo cd /d %%~dp0 >> logcatch.bat
+  echo start "-" /B "%BASH_PATH%" -l -c "wish '%%~dp0LogCatch.tcl' %%*" >> logcatch.bat
+)
+if exist path_list.tmp del path_list.tmp
 
 
 set AWK_CMD="awk.exe"
-dir /s /b %SEARCH_PATH% | findstr "%AWK_CMD%$" > path_list.tmp
-for /f "delims=" %%i in (path_list.tmp) do (
-  echo "%%i"
-  echo %%i>> %PATH_LIST%
-  echo %%i> awk_path.list
-  break
+set AWK_PATH=
+dir /s /b %SEARCH_PATH% 2>nul | findstr "%AWK_CMD%$" > path_list.tmp
+if exist path_list.tmp set /p AWK_PATH=<path_list.tmp
+if defined AWK_PATH (
+  echo "%AWK_PATH%"
+  echo %AWK_PATH%>> %PATH_LIST%
+  echo %AWK_PATH%> awk_path.list
 )
-del path_list.tmp
+if exist path_list.tmp del path_list.tmp
 
 
-@rem set /P ans=""
-@rem echo %ans%
+if not exist %PATH_LIST% (
+  echo.
+  echo ERROR: found none of wish.exe / bash.exe / awk.exe under %SEARCH_PATH%
+  echo Install Git for Windows, or edit SEARCH_PATH at the top of this script.
+  exit /b 1
+)
+if not defined AWK_PATH (
+  echo.
+  echo WARNING: awk.exe not found. LogCatch needs GNU Awk 5.0 or newer to filter logs.
+)
+if not defined WISH_FOUND if not defined BASH_PATH (
+  echo.
+  echo WARNING: neither wish.exe nor bash.exe found, logcatch.bat was not generated.
+)
 
 exit /b 0
-
